50bc3728d2eae4efeec110acb3e7a623897f2b7d
Vaibhav Bhagwat <90514298+vaibhav1881@users.noreply.github.com>
2022-11-19 22:11:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ num = StringVar()
 
 # Adding title
 title = Label(root, text="Number to Words converter",
-               fg="Black", font=("Times New Roman", 20, 'bold')).place(x=220, y=10)
+               fg="black", font=("Times New Roman", 20, 'bold')).place(x=220, y=10)
 
 # Options
 formats_lable = Label(root, text="Formats supported :  ",
